2c68352d582334ba4576e76c0d8be5a6605cf4bd
tct123 <42028373+tct123@users.noreply.github.com>
2026-09-13 01:24:11 +0200

    Update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -21,8 +21,7 @@ export default function RootLayout() {
     <FavouritesProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <NativeTabs
-          backgroundColor={backgroundColor}
-          disableTransparentOnScrollEdge={!hasLiquidGlass}>
+          backgroundColor={backgroundColor}>
           <NativeTabs.Trigger name="(flavours)">
             <NativeTabs.Trigger.Icon sf="cup.and.saucer" md="local_cafe" />
             <NativeTabs.Trigger.Label>Flavours</NativeTabs.Trigger.Label>
